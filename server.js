cccb38fda213bfec4fe30063bd416fed26aab2ca
Subrat Hati <subratdude98@gmail.com>
2025-04-18 13:12:31 +0530

    Fix: corrected User model path and updated server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const User = require('./models/User');
+const User = require('./models/user');
 const app = express();
 const PORT = 3000;
 
